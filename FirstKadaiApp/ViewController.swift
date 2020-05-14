da7acc0d42194ce04138c579661ca2b2e44e0e2e
kenji.A <kenji.25a@gmail.com>
2020-05-14 19:10:21 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by user on 2020/05/14.
+//  Copyright © 2020 Kenji.A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
